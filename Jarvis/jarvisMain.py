ebc51ac439ac1e8fffd26f23e9cb6fe0e9676ca4
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-02-04 19:06:06 +0800

    Added listening and speech

--- Jarvis/jarvisMain.py
+++ Jarvis/jarvisMain.py
@@ -1,11 +1,24 @@
+import re
 import pyttsx3
+import speech_recognition
 
 def main():
     print("Welcome to Jarvis Program")
+    #Initialise the Jarvis program
     engine = pyttsx3.init()
+    engine.setProperty('rate', 145)
     engine.say("Welcome to Jarvis Program")
-    print("Testing Push")
-    engine.runAndWait()
+
+    #Initialise Listening
+    recognizer = speech_recognition.Recognizer()
+    with speech_recognition.Microphone() as source:
+        print("Jarvis is Listening: ")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+    
+    #Recognize speech using Google Speech Recognition
+    print(recognizer.recognize_google(audio))
+    
 
 if __name__ == "__main__":
     main()
