c7a40bb680bd8c5fd1c9f62bb423160e4688a688
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-02-04 18:35:36 +0800

    Install text to speech and speech to text

--- Jarvis/jarvisMain.py
+++ Jarvis/jarvisMain.py
@@ -1,6 +1,11 @@
+import pyttsx3
+
 def main():
     print("Welcome to Jarvis Program")
+    engine = pyttsx3.init()
+    engine.say("Welcome to Jarvis Program")
     print("Testing Push")
+    engine.runAndWait()
 
 if __name__ == "__main__":
     main()
