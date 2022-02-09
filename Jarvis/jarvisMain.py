cb2813ab82d3652da2796cfc22fe94c36c0a45b5
stevenpyae <steven.pyaelinkyaw@gmail.com>
2022-02-09 14:53:00 +0800

    Fix

--- Jarvis/jarvisMain.py
+++ Jarvis/jarvisMain.py
@@ -8,12 +8,14 @@ def main():
     engine = pyttsx3.init()
     engine.setProperty('rate', 145)
     engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
-
+    
+    #Create Instance of Friday
     fridayInstance = friday.Friday()
     words = fridayInstance.listen()
     matches = re.search("Friday", words)
 
     if matches:
+        #If Friday is called
         engine.say("Yes Sir!, I am listening")
         engine.runAndWait()
         newwords = fridayInstance.listen()
